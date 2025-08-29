916ec3e0c2ef5ecfa1ab1b6df3de68ab02551866
Huy Cuong <cuong.pham0212@gmail.com>
2025-08-30 00:38:32 +0700

    fix preview admin, fix file cms.js

--- static/admin/cms.js
+++ static/admin/cms.js
@@ -1,8 +1,14 @@
 const PostPreview = ({ entry, widgetFor }) => {
+  // Lấy dữ liệu từ entry
+  const title = entry.getIn(["data", "title"]);
+  const description = entry.getIn(["data", "description"]);
+  const body = widgetFor("body");
+
   return (
-    <div style={{ padding: "1rem", fontFamily: "sans-serif" }}>
-      <h1>{entry.getIn(["data", "title"])}</h1>
-      <div>{widgetFor("body")}</div>
+    <div style={{ fontFamily: "sans-serif", padding: "1rem" }}>
+      <h1>{title}</h1>
+      {description && <p><em>{description}</em></p>}
+      <div>{body}</div>
     </div>
   );
 };
