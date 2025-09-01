ba43953a09927dcf9605c279afbf2a4105514ac1
cuongpham0212 <cuong.pham0212@gmail.com>
2025-09-01 09:44:48 +0700

    Create main.js

---
+++ static/Js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.getElementById("menu-toggle");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  if (toggleBtn && mobileMenu) {
+    toggleBtn.addEventListener("click", () => {
+      mobileMenu.classList.toggle("hidden");
+    });
+  }
+});
