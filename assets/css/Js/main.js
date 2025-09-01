b6b14c34c6fc7986791fdb42f600860b1a16c207
cuongpham0212 <cuong.pham0212@gmail.com>
2025-09-01 09:35:53 +0700

    Create main.js

---
+++ assets/css/Js/main.js
@@ -0,0 +1,11 @@
+// assets/js/main.js
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.getElementById("menu-toggle");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  if (toggleBtn && mobileMenu) {
+    toggleBtn.addEventListener("click", () => {
+      mobileMenu.classList.toggle("hidden");
+    });
+  }
+});
